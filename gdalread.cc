2df618275ed72d0214806c77a047b20117519467
shashank <skhare@hotmail.com>
2015-07-15 14:18:03 +0530

    data file to test script added

--- gdalread.cc
+++ gdalread.cc
@@ -1,8 +1,10 @@
 /*
 The MIT License (MIT)
 
-Copyright (C) 2015 Shashank Khare
+Copyright (C) 2015 Shashank Khare, skhare at hotmail dot com
 
+Acknowledgement: Snow and Avalanche Studies Establishment, DRDO, Chandigarh, India
+ 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
@@ -23,7 +25,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 void print_usage()
 {
-	octave_stdout << "GDALRead expects two arguments - geotiff filename and data output format.\n";
+	octave_stdout << "GDALRead expects one argument - gdal compatible filename and output two arguments - cell array containing band data and struct containing metadata (spatial and nonspatial)\n";
 }
 /*
 INPUT : string filepath
--- makefile
+++ makefile
@@ -1,12 +1,12 @@
-project : shaperead.oct gdalread.oct gdalwrite.oct
+project : gdalshaperead.oct gdalread.oct gdalwrite.oct
 
-shaperead.oct : shaperead.cc misc.cpp misc.h
-	mkoctfile -lgdal -o shaperead.oct shaperead.cc misc.cpp
+gdalshaperead.oct : gdalshaperead.cc misc.cpp misc.h
+	mkoctfile -lgdal -o gdalshaperead.oct gdalshaperead.cc misc.cpp
 gdalread.oct : gdalread.cc misc.cpp misc.h
 	mkoctfile -lgdal -o gdalread.oct gdalread.cc misc.cpp
 gdalwrite.oct: gdalwrite.cc 
 	mkoctfile -lgdal -o gdalwrite.oct gdalwrite.cc
 
 clean:
-	rm gdalwrite.o misc.o shaperead.o gdalread.o
+	rm gdalwrite.o misc.o gdalshaperead.o gdalread.o
 
